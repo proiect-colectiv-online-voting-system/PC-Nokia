04f892eef719ca546e58cf4a43b5cfdd3896f349
Marius Ioan Orban <marius.orban99@gmail.com>
2019-04-01 23:51:41 +0300

    Added user login and poll list endpoints

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var mongoose = require('mongoose');  // module for easy MongoDB connection
 var db = require('./db');
 
 var indexRouter = require('./routes/index');
+var userRouter  = require('./routes/api/user')
 var adminRouter = require('./routes/api/admin');
 
 var app = express();
@@ -30,6 +31,7 @@ mongoose.connect(db.mongo_port, {useNewUrlParser: true})
 // routes to use
 app.use('/', indexRouter);
 app.use('/api/admin', adminRouter);
+app.use('/api/', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,8 +45,10 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
