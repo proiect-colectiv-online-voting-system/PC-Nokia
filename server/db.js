7413c07b99a0976e45b2e78e1167221aafa4fcaf
Marius Ioan Orban <marius.orban99@gmail.com>
2019-05-16 21:12:57 +0300

    Added connection string for remote DB

--- server/db.js
+++ server/db.js
@@ -1,3 +1,3 @@
-const mongo_port = "mongodb://localhost:27017/voting_system_test";
+const mongo_port = "mongodb://client_v1:soimiipatriei@92.87.91.15:27017/virtualvote_v1";
 
 module.exports = {mongo_port}
