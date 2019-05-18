ff53ec9934c3c33bc2155484e1ebb5cb74ec53bd
Marius Ioan Orban <marius.orban99@gmail.com>
2019-05-18 19:58:46 +0300

    Added CORS handling package

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan'); 
 var mongoose = require('mongoose');  // module for easy MongoDB connection
 var db = require('./db');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var userRouter  = require('./routes/api/user')
@@ -19,6 +20,7 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
